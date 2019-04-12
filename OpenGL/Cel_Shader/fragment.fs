1a5da87a66951af1800b7ae3fae5147497b409e0
Scott Nicholes <scott93gps@gmail.com>
2019-04-12 17:36:01 -0600

    Made changes to make double-pass

--- OpenGL/Cel_Shader/fragment.fs
+++ OpenGL/Cel_Shader/fragment.fs
@@ -13,7 +13,7 @@ varying vec3 v_Normal;
 varying vec3 v_Position;
 
 // Cel Shader
-const float levels = 2.0;
+const float levels = 3.0;
 const float scaleFactor = 1.0 / levels;
 const vec3 diffuseColor = vec3(0.30,0.80,0.10);
 
@@ -29,35 +29,37 @@ void main()
     // vec3 diffuse = diff * (lightColor * u_DiffuseStrength);
 
     // Cel Diffuse
-    vec3 diffuse = (u_ObjectColor * u_DiffuseStrength) * floor(diff * levels) * scaleFactor;
+    vec3 diffuse = (lightColor * u_DiffuseStrength) * floor(diff * levels) * scaleFactor;
 
-    vec3 V = normalize(u_CameraPosition - v_Position);
-    vec3 H = normalize(lightDirection + V);
+    // vec3 V = normalize(u_CameraPosition - v_Position);
 
     // Specular Lighting
     vec3 viewDirection = normalize(u_CameraPosition - v_Position);
     vec3 reflectDirection = reflect(-lightDirection, norm);
+    vec3 H = normalize(lightDirection + viewDirection);
 
     // Cel Specular
-    float specularFloat = 0.0;
-    if (dot(lightDirection, v_Normal) > 0.0) {
-        specularFloat = pow(max(dot(viewDirection, norm), 0.0), 32.0);
+    vec3 specular = vec3(0.0, 0.0, 0.0);
+    if (dot(lightDirection, norm) > 0.0) {
+        specular = u_SpecularStrength * pow(max(dot(viewDirection, norm), 0.0), 120.0);
     }
     // limit specular
-    float specMask = (pow(dot(H, norm), 32.0) > 0.2) ? 1.0 : 0.0;
-    float edgeDetection = (dot(V, norm) > 0.4) ? 1.0 : 0.0;
+    float specMask = (pow(dot(H, norm), 120.0) > 0.4) ? 1.0 : 0.0;
+    // float edgeDetection = (dot(viewDirection, norm) > 0.4) ? 1.0 : 0.0;
 
-    float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), 32.0);
+    float spec = pow(max(dot(viewDirection, norm), 0.0), 120.0);
     // vec3 specular = u_SpecularStrength * spec * lightColor;
 
-    // vec4 sample = texture2D(u_Texture, v_UV);
+    vec4 sample = texture2D(u_Texture, v_UV);
 
-    // if (sample.r > u_Threshold)
-    // {
-    //     discard;
-    // }
+    if (sample.r > u_Threshold)
+    {
+        discard;
+    }
 
     // vec3 result = (ambient + diffuse + specular) * u_ObjectColor;
-    vec3 result = edgeDetection * (u_ObjectColor + diffuse + specularFloat * specMask);
-    gl_FragColor = vec4(result, 1.0);
+    // vec3 result = edgeDetection * (u_ObjectColor + diffuse + specularFloat * specMask);
+    vec4 result = (vec4(u_ObjectColor, 1.0) * vec4(diffuse, 1.0) + vec4(specular, 1.0) * specMask) * sample;
+    // gl_FragColor = vec4(result, 1.0);
+    gl_FragColor = result;
 }
